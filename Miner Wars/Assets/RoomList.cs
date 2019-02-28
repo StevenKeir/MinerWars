942b1a12697ddb2c5f98b381098cf49ef92a627e
Steven <1011099@student.sae.edu.au>
2019-03-01 00:26:37 +0800

    update

---
+++ Miner Wars/Assets/RoomList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class RoomList : MonoBehaviour
+{
+    //public PhotonView pv;
+    public static RoomList roomList;
+
+
+    private void OnEnable() 
+    {
+        roomList = this;
+    }
+
+    private void Update() {
+        print(PhotonNetwork.CountOfRooms);
+    }
+   
+   
+
+
+}
