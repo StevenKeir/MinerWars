9fb6b026eb9a9057045c75d8e26e942bda7c2090
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2019-03-23 03:06:07 +0800

    Update
    - Started shop intergration

---
+++ Miner Wars/Assets/ShopMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopMenu : MonoBehaviour
+{
+
+
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.F))
+        {
+            print("Shop opened by " + collision.gameObject.name);
+        }
+    }
+
+
+}
