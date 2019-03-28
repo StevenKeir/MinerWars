3f4a088e9f2bf2001e77eea01f47bfd13e1bbd7b
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2019-03-28 19:05:02 +0800

    update
    - made TMP font
    - testing score sending

---
+++ Miner Wars/Assets/Scripts/GameControllers/ScoreCounter.cs
@@ -0,0 +1,45 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+
+    public int[] scoreList = new int[2];
+    public int localScore;
+    public PhotonView PV;
+
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    private void Update()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            scoreList[0] = localScore;
+            PV.RPC("RPC_SendScore", RpcTarget.Others, scoreList[0]);
+        }
+        else
+        {
+            scoreList[1] = localScore;
+            PV.RPC("RPC_SendScore", RpcTarget.MasterClient, scoreList[1]);
+        }
+    }
+
+
+
+
+
+    [PunRPC]
+    void RPC_SendScore(int playerScore)
+    {
+        scoreList[1] = playerScore;
+    }
+
+
+
+}
