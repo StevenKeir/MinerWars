feeadf3e403b1f8ec0ef399446e1a3b3f53148bf
StevenKeir <39974047+StevenKeir@users.noreply.github.com>
2019-04-03 02:29:22 +0800

    Update Rock.cs
    - fixed issue with not destroying the rock properly

--- Miner Wars/Assets/Scripts/ItemRelated/Rock.cs
+++ Miner Wars/Assets/Scripts/ItemRelated/Rock.cs
@@ -10,6 +10,7 @@ public class Rock : MonoBehaviour
     public int randomValue;
     bool hit;
 
+    //Initialize the references and setting the random value for which it uses to put out a random gold piece. also setting hit to false to it will set to true only is hit.
     private void Awake()
     {
         PV = GetComponent<PhotonView>();
@@ -17,38 +18,35 @@ public class Rock : MonoBehaviour
         hit = false;
     }
 
-
+    //Checks if the rock was hit if so sends to the masterclient to destroy the object, then since it has a PhotonView it deletes it on both clients.
     private void Update()
     {
-
-            if (hit)
+        //Was in a if(PhotonNetwork.isMasterClient) but had issues with errors saying it couldn't destory the object, even tho it did it anyway.
+        if (hit)
+        {
+            if (randomValue >= 9)
             {
-                if (randomValue >= 9)
-                {
-                    PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "GoldBig"), transform.position, Quaternion.identity, 0);
-                }
-                else if (randomValue >= 7 && randomValue <= 8)
-                {
-                    PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "GoldSmall"), transform.position, Quaternion.identity, 0);
-                }
-                else if (randomValue >= 3 && randomValue <= 6)
-                {
-                    PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "GoldMedium"), transform.position, Quaternion.identity, 0);
-                }
-                else
-                {
-                    //print("No gold found");
-                }
-                PV.RPC("RPC_DestroyMe", RpcTarget.MasterClient);
-                hit = false;
-
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "GoldBig"), transform.position, Quaternion.identity, 0);
+            }
+            else if (randomValue >= 7 && randomValue <= 8)
+            {
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "GoldSmall"), transform.position, Quaternion.identity, 0);
+            }
+            else if (randomValue >= 3 && randomValue <= 6)
+            {
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "GoldMedium"), transform.position, Quaternion.identity, 0);
+            }
+            else
+            {
+                //print("No gold found");
+            }
+            PV.RPC("RPC_DestroyMe", RpcTarget.MasterClient);
+            hit = false;
         }
     }
 
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.gameObject.tag == "Explosion")
         {
             hit = true;
