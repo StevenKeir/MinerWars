558a1a9b772bfae7d64857b1cf4532cc9daa54c7
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2019-04-03 10:18:31 +0800

    Menu Audio
    Nathan

---
+++ Miner Wars/Assets/Audio/ButtonAudio.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour
+{
+    public AudioSource mouseOverNoise;
+    public AudioSource selectNoise;
+    public AudioSource skinHover;
+    public AudioSource skinSelectwork;
+    public AudioSource skinSelectFail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseOver()
+    {
+        mouseOverNoise.Play();
+    }
+
+    public void OnMouseDown()
+    {
+        selectNoise.Play();
+    }
+
+    public void SkinOnMouseOver()
+    {
+        skinHover.Play();
+    }
+
+    public void SkinSelectOnMouseDown()
+    {
+        skinSelectwork.Play();
+    }
+
+    public void SkinFailOnMouseDown()
+    {
+        skinSelectFail.Play();
+    }
+}
