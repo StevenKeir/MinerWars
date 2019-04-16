5ff410b38a0cfd431e7f83562fa73c0daff67ef1
NathanBorona <39974058+NathanBorona@users.noreply.github.com>
2019-04-16 19:20:24 +0800

    LotsAStuff
    Nathan

---
+++ Miner Wars/Assets/Audio/FootstepsRandom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VocalsRandom : MonoBehaviour {
+
+    public AudioSource[] footsteps;
+    public int howManySteps;
+    public int stepNumber;
+    public float timer;
+    public float stepTime;
+    public float minTime = 1;
+    public float maxTime = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            stepNumber = Random.Range(0, howManySteps);
+        stepTime = Random.Range(minTime, maxTime);
+            timer += Time.deltaTime;
+            if (timer >= stepTime)
+            {
+                footsteps[stepNumber].Play();
+                timer = 0;
+            }
+        }
+
+}
---
+++ Miner Wars/Assets/Audio/Vocals.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vocals : MonoBehaviour {
+
+    public AudioSource[] footsteps;
+    public int howManySteps;
+    public int stepNumber;
+    public float timer;
+    public float stepTime;
+    public float minTime = 1;
+    public float maxTime = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        stepTime = Random.Range(minTime, maxTime);
+        stepNumber = Random.Range(0, howManySteps);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        timer += Time.deltaTime;
+
+        if (timer >= stepTime)
+        {
+            footsteps[stepNumber].Play();
+            stepNumber = Random.Range(0, howManySteps);
+            stepTime = Random.Range(minTime, maxTime);
+            timer = 0;
+        }
+    }
+
+}
